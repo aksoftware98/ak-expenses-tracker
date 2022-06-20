6b105a6ab5883d5dc66f50d2f778f5336194a143
Ahmad Mozaffar <aksoftware98@hotmail.com>
2022-06-20 18:05:30 +0400

    Improve the startup.cs file of the Azure Functions

--- src/AKExpnesesTracker/AKExpnesesTracker.Server.Functions/Startup.cs
+++ src/AKExpnesesTracker/AKExpnesesTracker.Server.Functions/Startup.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
+[assembly: FunctionsStartup(typeof(AKExpnesesTracker.Server.Functions.Startup))]
 namespace AKExpnesesTracker.Server.Functions
 {
-    internal class Startup
+    public class Startup : FunctionsStartup
     {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            
+        }
     }
 }
