36d2fa6e4fe4193c807842c7f0c6ae78566301b1
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-06-07 17:39:00 +0400

    Create the TransactionDto and Transaction Fluent Validator

---
+++ src/AKExpnesesTracker/AKExpensesTracker.Shared/DTOs/TransactionDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKExpensesTracker.Shared.DTOs;
+
+public class TransactionDto
+{
+
+    public TransactionDto()
+    {
+		Category = "Other";
+		WalletId = string.Empty; 
+    }
+
+    public string? Id { get; set; }
+
+	public string? Description { get; set; }
+
+	public decimal Amount { get; set; }
+
+	public DateTime? DateTime { get; set; }
+
+	public string Category { get; set; }
+
+	public string[]? Tags { get; set; }
+	public string[]? Attachments { get; set; }
+
+	public bool IsIncome { get; set; }
+
+	public string WalletId { get; set; }
+}
---
+++ src/AKExpnesesTracker/AKExpensesTracker.Shared/Validators/TransactionDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using AKExpensesTracker.Shared.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKExpensesTracker.Shared.Validators;
+
+public class TransactionDtoValidator : AbstractValidator<TransactionDto>
+{
+
+    public TransactionDtoValidator()
+    {
+        RuleFor(x => x.Amount)
+            .GreaterThanOrEqualTo(0).WithMessage("Amount cannot be negative");
+
+        RuleFor(x => x.WalletId)
+            .NotEmpty().WithMessage("Wallet is required");
+
+        RuleFor(x => x.Category)
+            .NotEmpty().WithMessage("Category is required");
+
+        RuleFor(x => x.DateTime)
+            .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Date cannot be in the future")
+            .When(x => x.DateTime != null);
+    }
+
+}
