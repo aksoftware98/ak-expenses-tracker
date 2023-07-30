28cf5fa05f854d9fc47ced445bad1388b596d2b2
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-07-30 16:58:23 +0400

    Implement the update transaction

--- src/AKExpnesesTracker/AKExpensesTracker.Server.Functions/UpsertTransaction.cs
+++ src/AKExpnesesTracker/AKExpensesTracker.Server.Functions/UpsertTransaction.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Threading.Tasks;
 using AKExpensesTracker.Server.Data.Interfaces;
 using AKExpensesTracker.Server.Data.Models;
+using AKExpensesTracker.Server.Functions.Services;
 using AKExpensesTracker.Shared.DTOs;
 using AKExpensesTracker.Shared.Responses;
 using FluentValidation;
@@ -21,32 +22,35 @@ using Newtonsoft.Json;
 
 namespace AKExpensesTracker.Server.Functions
 {
-    public class UpsertTransaction
-    {
-        private readonly ILogger<UpsertTransaction> _logger;
-        private readonly ITransactionsRepository _transactionsRepo;
+	public class UpsertTransaction
+	{
+		private readonly ILogger<UpsertTransaction> _logger;
+		private readonly ITransactionsRepository _transactionsRepo;
 		private readonly IWalletsRepository _walletsRepo;
 		private readonly IAttachmentsRepository _attachmentsRepo;
 		private readonly IValidator<TransactionDto> _validator;
+		private readonly IStorageService _storageService;
 		public UpsertTransaction(ILogger<UpsertTransaction> log,
 								 ITransactionsRepository transactionsRepo,
 								 IWalletsRepository walletsRepo,
 								 IAttachmentsRepository attachmentsRepo,
-								 IValidator<TransactionDto> validator)
+								 IValidator<TransactionDto> validator,
+								 IStorageService storageService)
 		{
 			_logger = log;
 			_transactionsRepo = transactionsRepo;
 			_walletsRepo = walletsRepo;
 			_attachmentsRepo = attachmentsRepo;
 			_validator = validator;
+			_storageService = storageService;
 		}
 
 		[FunctionName("UpsertTransaction")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+		[OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+		[OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+		public async Task<IActionResult> Run(
+			[HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
 		{
 			_logger.LogInformation("C# HTTP trigger function processed a request.");
 			_logger.LogInformation("Upsert wallet started");
@@ -60,6 +64,52 @@ namespace AKExpensesTracker.Server.Functions
 
 			if (isUpdate)
 			{
+				var transaction = await _transactionsRepo.GetByIdAsync(data.Id, userId, data.CreationDate.Year);
+				if (transaction == null)
+					return new NotFoundResult();
+
+				var existingAttachments = transaction.Attachments;
+				var newAttachments = data.Attachments;
+
+				// New Attachments			 EXisting Attachments
+				//  1							1
+				//  2							2
+				//  4	Add						3 Delete
+				//								5 Delete
+
+				var attachmentsToDelete = existingAttachments?.Except(newAttachments);
+				var attachmentsToAdd = newAttachments?.Except(existingAttachments);
+
+				foreach (var item in attachmentsToDelete)
+				{
+					await _storageService.DeleteFileAsync(item);
+				}
+
+				if (attachmentsToAdd.Any())
+				{
+					var newlyAttachments = await _attachmentsRepo.GetByURLsAsync(attachmentsToAdd.ToArray());
+					var deleteTasks = new List<Task>();
+					// Delete from the temp attachments table
+					foreach (var item in newlyAttachments)
+					{
+						var task = _attachmentsRepo.DeleteAsync(item.Id, userId);
+					}
+
+					await Task.WhenAll(deleteTasks);
+				}
+
+				// Update the transaction 500
+				var amountToModify = transaction.IsIncome ? -transaction.Amount : transaction.Amount;
+				
+				transaction.Update(data.Amount,
+								   data.Category,
+								   data.IsIncome,
+								   data.Description,
+								   data.Tags,
+								   data.Attachments?.ToArray());
+
+				var newAmount = data.IsIncome ? data.Amount : -data.Amount;
+
 
 			}
 			else
